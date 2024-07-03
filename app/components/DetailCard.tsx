ebf9aa0cff70d81902d26788a9790b739498c658
rubuz <rubuz23@gmail.com>
2024-07-03 14:51:00 +0200

    Update CustomButton component to add disable prop

---
+++ app/components/DetailCard.tsx
@@ -0,0 +1,83 @@
+import { View, Text, Image, TouchableOpacity, TextInput } from "react-native";
+import { LastnikOSResult } from "../../types/types";
+
+const chevronLeft = require("../../assets/images/chevron-left.png");
+
+interface DetailCardProps {
+  dataOS: LastnikOSResult | null;
+  novaLokacija: string;
+  popisanoColor: boolean;
+  handleNovaLokacija: (e: string) => void;
+  handleCopy: () => void;
+}
+
+const DetailCard = ({
+  dataOS,
+  novaLokacija,
+  popisanoColor,
+  handleNovaLokacija,
+  handleCopy,
+}: DetailCardProps) => {
+  return (
+    <View
+      className={`p-4 my-4 rounded-2xl ${
+        popisanoColor ? "bg-green-200" : "bg-red-200"
+      }`}
+    >
+      <Text className="text-lg font-pmedium">{dataOS?.osstanje_ime}</Text>
+      <Text className="text-sm font-psemibold">Lastnik</Text>
+      <View className="w-full flex flex-row justify-between">
+        <Text className="font-pregular text-lg">{dataOS?.ime}</Text>
+        <Text>{dataOS?.sifra}</Text>
+      </View>
+      <Text className="text-sm mt-2 font-psemibold">Lokacija</Text>
+      <View className="w-full flex flex-row justify-between">
+        <Text className="font-pregular text-lg">{dataOS?.lokacija}</Text>
+        <Text>{dataOS?.obrat_ime}</Text>
+      </View>
+      <View className="mt-2 w-full relative">
+        <Text className="text-center mb-1 font-psemibold text-sm">
+          Nova lokacija
+        </Text>
+        <View
+          className={`w-full h-10 border-[1px] rounded-2xl flex-row items-center justify-between pl-4 relative ${
+            popisanoColor
+              ? "border-green-300 bg-green-100"
+              : "border-red-300 bg-red-100"
+          }`}
+        >
+          <TextInput
+            className="font-pregular flex h-full w-full items-center"
+            keyboardType="numeric"
+            value={novaLokacija}
+            onChangeText={handleNovaLokacija}
+          />
+          <TouchableOpacity
+            className={`h-full flex-row items-center justify-center ${
+              popisanoColor ? "bg-green-300" : "bg-red-300"
+            } rounded-[14px] px-4 absolute right-0 z-10`}
+            onPress={handleCopy}
+          >
+            <Image
+              source={chevronLeft}
+              resizeMode="contain"
+              className="h-3 w-3 pr-6"
+            />
+            <Text className="text-sm font-pregular">Kopiraj staro</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      {dataOS?.popisan === "D" ? (
+        <Text className="text-base font-pbold text-green-700 text-center mt-4">
+          POPISANO
+        </Text>
+      ) : (
+        <Text className="text-base font-pbold text-red-700 text-center mt-2">
+          NEPOPISANO
+        </Text>
+      )}
+    </View>
+  );
+};
+
+export default DetailCard;
--- app/home.tsx
+++ app/home.tsx
@@ -11,6 +11,8 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { useRouter } from "expo-router";
 import { getOSinfo } from "../api/apiService";
 import { LastnikOSResponse, LastnikOSResult } from "../types/types";
+import DetailCard from "./components/DetailCard";
+import CustomButton from "./components/CustomButton";
 
 const chevronLeft = require("../assets/images/chevron-left.png");
 
@@ -47,7 +49,7 @@ export default function home() {
       <ScrollView>
         <View className="w-full min-h-[85vh] justify-center px-4 my-6 font-pregular">
           <Text className="text-2xl font-psemibold mt-5 text-center">
-            Invetura OS
+            Inventura OS
           </Text>
           <View className="w-full h-16 px-4 pl-6 mt-6 bg-white border-2 border-slate-600 rounded-2xl focus:border-blue-700 flex-row items-center">
             <TextInput
@@ -58,56 +60,19 @@ export default function home() {
               onSubmitEditing={() => handleGetLastnik()}
             />
           </View>
-          <View
-            className={`p-4 my-4 rounded-2xl ${
-              dataOS?.popisan === "D" ? "bg-green-200" : "bg-red-200"
-            }`}
-          >
-            <Text className="text-lg font-pmedium">{dataOS?.osstanje_ime}</Text>
-            <Text className="text-sm font-psemibold">Lastnik</Text>
-            <View className="w-full flex flex-row justify-between">
-              <Text className="font-pregular text-lg">{dataOS?.ime}</Text>
-              <Text>{dataOS?.sifra}</Text>
-            </View>
-            <Text className="text-sm mt-2 font-psemibold">Lokacija</Text>
-            <View className="w-full flex flex-row justify-between">
-              <Text className="font-pregular text-lg">{dataOS?.lokacija}</Text>
-              <Text>{dataOS?.obrat_ime}</Text>
-            </View>
-            <View className="mt-2 w-full relative">
-              <Text className="text-center mb-1 font-psemibold text-sm">
-                Nova lokacija
-              </Text>
-              <View className="w-full h-10 border-[1px] border-red-300 rounded-2xl bg-red-100 flex-row items-center justify-between pl-4 relative">
-                <TextInput
-                  className="font-pregular flex h-full w-full items-center"
-                  keyboardType="numeric"
-                  value={novaLokacija}
-                  onChangeText={(e) => setNovaLokacija(e)}
-                />
-                <TouchableOpacity
-                  className="h-full flex-row items-center justify-center bg-red-300 rounded-[14px] px-4 absolute right-0 z-10"
-                  onPress={handleCopyStaro}
-                >
-                  <Image
-                    source={chevronLeft}
-                    resizeMode="contain"
-                    className="h-3 w-3 pr-6"
-                  />
-                  <Text className="text-sm font-pregular">Kopiraj staro</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-            {dataOS?.popisan === "D" ? (
-              <Text className="text-base font-pbold text-green-700 text-center mt-4">
-                POPISANO
-              </Text>
-            ) : (
-              <Text className="text-base font-pbold text-red-700 text-center mt-2">
-                NEPOPISANO
-              </Text>
-            )}
-          </View>
+          <DetailCard
+            dataOS={dataOS}
+            novaLokacija={novaLokacija}
+            popisanoColor={dataOS?.popisan === "D" ? true : false}
+            handleNovaLokacija={(e: string) => setNovaLokacija(String(e))}
+            handleCopy={handleCopyStaro}
+          />
+          <CustomButton
+            title="VNESI"
+            handlePress={() => console.log("VNEŠENO")}
+            containerStyles="bg-[#002d5f]"
+            disable={novaLokacija === "0" ? true : false}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
