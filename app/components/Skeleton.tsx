a1e770fbae8ec07471c0dcc2c49d4bae81fc292f
rubuz <rubuz23@gmail.com>
2024-07-25 08:01:35 +0200

    refactor: Update Skeleton component to use new dimensions and colors

--- app/components/Skeleton.tsx
+++ app/components/Skeleton.tsx
@@ -10,15 +10,19 @@ const Skeleton = () => {
         height={200}
         viewBox="0 0 500 200"
         backgroundColor="#f3f3f3"
-        foregroundColor="#000000"
+        foregroundColor="#cccccc"
       >
-        <Rect x="15" y="10" rx="10" ry="10" width="250" height="20" />
+        <Rect x="45" y="10" rx="10" ry="10" width="200" height="20" />
 
-        <Rect x="25" y="45" rx="5" ry="5" width="250" height="15" />
+        <Rect x="15" y="45" rx="10" ry="10" width="150" height="15" />
 
-        <Rect x="25" y="75" rx="5" ry="5" width="250" height="15" />
+        <Rect x="15" y="75" rx="10" ry="10" width="150" height="15" />
 
-        <Rect x="25" y="105" rx="5" ry="5" width="250" height="15" />
+        <Rect x="15" y="105" rx="10" ry="10" width="150" height="15" />
+
+        <Rect x="15" y="135" rx="10" ry="10" width="150" height="15" />
+
+        <Rect x="65" y="170" rx="10" ry="10" width="170" height="15" />
       </ContentLoader>
     </>
   );
